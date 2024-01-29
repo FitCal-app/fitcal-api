ecf09b1f1c20edab010289e477808824bc2b3f7d
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-01-29 16:12:30 +0100

    Added mongoDB connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 // routes
@@ -6,6 +7,15 @@ app.get('/', (req, res) => {
     res.send('Hello node API')
 })
 
-app.listen(3001, () => {
-    console.log('listening on port 3001')
+
+
+mongoose.connect('mongodb+srv://admin:zhhCF6PCtx7bMP6@fitcal.ywzg4zz.mongodb.net/FitCal-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Connected to MongoDB')
+    
+    app.listen(3001, () => {
+        console.log('Listening on port 3001')
+    })
+}).catch((err) => {
+    console.log(err)
 })
