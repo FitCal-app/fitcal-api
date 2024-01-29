f1222e8c5b44249577202dba557a6331aeff6bc6
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-01-29 19:39:18 +0100

    Added cors

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 const produstRoute = require('./routes/productRoute');
 const userRoute = require('./routes/userRoute');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
+const FRONTEND = process.env.FRONTEND;
 
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
