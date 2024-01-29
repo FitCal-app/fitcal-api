c1a58ff4f2a75b6419a375d64e80c7e539a0dd71
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-01-29 16:48:48 +0100

    Added update for products

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Product = require('./models/productModel');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // routes
 app.get('/', (req, res) => {
@@ -39,6 +40,25 @@ app.post('/products', async(req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product);
+    } catch (err) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
+
+// Update a single product              NON FUNZIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        // Cant find product in db with this id
+        if (!product) {
+            return res.status(404).json({message: 'Product not found with ID ${id}'});
+        }
+
+        const updatedProduct =  await Product.findById(id);
+        res.status(200).json(updatedProduct);
     } catch (err) {
         console.log(error.message);
         res.status(500).json({message: error.message});
