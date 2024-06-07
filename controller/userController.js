182bf602dc8054c303effac354db5d1f87b9f11a
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-06-07 22:44:24 +0200

    Added redis test

--- controller/userController.js
+++ controller/userController.js
@@ -1,16 +1,35 @@
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
+const Redis = require('ioredis');
+// Create Redis client using IORedis
+const redisClient = new Redis(process.env.REDIS_URL);
+redisClient.on('error', (error) => console.error(`Redis Error: ${error}`));
 
 // Get single user by Clerk ID
 const getUserByClerkUserId = asyncHandler(async (req, res) => {
     try {
         const { clerkUserId } = req.params;
+
+        // Check if user exists in Redis
+        let cachedUser = await redisClient.get(clerkUserId);
+
+        if (cachedUser) {
+            res.status(200).json(JSON.parse(cachedUser));
+            return;
+        }
+
+        // If not in Redis, fetch from MongoDB
         const user = await User.findOne({ clerkUserId });
+
         if (!user) {
             res.status(404);
             throw new Error(`Cannot find any user with Clerk User ID ${clerkUserId}`);
         }
+        
+        // Store the user in Redis for future requests
+        await redisClient.set(clerkUserId, JSON.stringify(user));
+
         res.status(200).json(user);
     } catch (err) {
         res.status(500);
@@ -55,6 +74,9 @@ const updateUserByClerkUserId = asyncHandler(async (req, res) => {
         const { clerkUserId } = req.params;
         const updatedUser = await User.findOneAndUpdate({ clerkUserId }, req.body, { new: true });
 
+        // Update user in redis as well
+        await redisClient.set(clerkUserId, JSON.stringify(updatedUser));
+
         if (!updatedUser) {
             res.status(404);
             throw new Error(`Cannot find any user with Clerk User ID ${clerkUserId}`);
@@ -90,6 +112,9 @@ const deleteUserByClerkUserId = asyncHandler(async (req, res) => {
     try {
         const { clerkUserId } = req.params;
         const user = await User.findOneAndDelete({ clerkUserId });
+
+        // Delete user from redis as well
+        await redisClient.del(clerkUserId);
 
         if (!user) {
             res.status(404);
