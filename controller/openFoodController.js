2673152842a340fd11fc1598833e199695c36473
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-06-07 23:27:45 +0200

    Added TTL to openfood redis set

--- controller/openFoodController.js
+++ controller/openFoodController.js
@@ -25,8 +25,8 @@ const getOpenFood = asyncHandler(async (req, res) => {
             throw new Error(`Cannot find any food with barcode ${barcode}`);
         }
         
-        // Store the food in Redis for future requests
-        await redisClient.set(barcode, JSON.stringify(data));
+        // Store the food in Redis for future requests with ttl 
+        await redisClient.set(barcode, JSON.stringify(data), 'EX', 600);
 
         res.status(200).json(data);
     } catch (err) {
