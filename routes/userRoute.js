d9e318389e3a9bdf536e3ff9bba23b26c32541ed
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-06-08 12:19:52 +0200

    Things

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getUserByClerkUserId, insertUser, updateUserByClerkUserId, deleteUserByClerkUserId } = require('../controller/userController')
+const { getUserByClerkUserId, updateUserByClerkUserId, deleteUserByClerkUserId } = require('../controller/userController')
 
 
 // Get usert by using clerkUserId
 router.get('/clerk/:clerkUserId', getUserByClerkUserId)
-
-// Insert a single user
-router.post('/', insertUser)
 
 // Update a single user by using clerkUserId
 router.patch('/clerk/:clerkUserId', updateUserByClerkUserId)
